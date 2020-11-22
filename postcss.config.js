8472cd32a598c4dc4ab62ad08363d12622e2a655
TheBoringDude <iamcoderx@gmail.com>
2020-11-22 18:24:52 +0800

    Re-configure npm packages and modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./**/*.njk'],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
+
 module.exports = {
   plugins: [
-    require(`tailwindcss`)(`./styles/tailwind.config.js`),
-    require(`autoprefixer`),
-    ...(process.env.NODE_ENV === "production"
-      ? [require(`postcss-clean`)]
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production'
+      ? [purgecss, require('cssnano')]
       : []),
   ],
-};
+}
